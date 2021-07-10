09d9b2be6ab217beb7ce56c61302391dff97b6c5
cb0806151 <cb0806151@otc.edu>
2021-07-10 18:07:58 -0500

    complete functioning version of part 2.3

--- index.mjs
+++ index.mjs
@@ -11,14 +11,21 @@ import * as backend from './build/index.main.mjs';
   const ctcAlice = alice.deploy(backend);
   const ctcBob = bob.attach(backend, ctcAlice.getInfo());
 
-  await Promise.all([
-    backend.Alice(ctcAlice, {
-      ...stdlib.hasRandom
-    }),
-    backend.Bob(ctcBob, {
-      ...stdlib.hasRandom
-    }),
-  ]);
+  const HAND = ['Rock', 'Paper', 'Scissors'];
+  const OUTCOME = ['Bob wins', 'Draw', 'Alice wins'];
+  const Player = (Who) => ({
+    getHand: () => {
+      const hand = Math.floor(Math.random() * 3);
+      console.log(`${Who} played ${HAND[hand]}`);
+      return hand;
+    },
+    seeOutcome: (outcome) => {
+      console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
+    },
+  });
 
-  console.log('Hello, Alice and Bob!');
+  await Promise.all([
+    backend.Alice(ctcAlice, Player('Alice')),
+    backend.Bob(ctcBob, Player('Bob')),
+  ]);
 })();
--- index.rsh
+++ index.rsh
@@ -1,9 +1,32 @@
 'reach 0.1';
 
+const Player = {
+  getHand: Fun([], UInt),
+  seeOutcome: Fun([UInt], Null)
+};
+
 export const main = Reach.App(() => {
-  const Alice = Participant('Alice', {});
-  const Bob   = Participant('Bob', {});
+  const A = Participant('Alice', {...Player});
+  const B   = Participant('Bob', {...Player});
   deploy();
-  // write your program here
+
+  A.only(() => {
+    const handA = declassify(interact.getHand());
+  });
+  A.publish(handA);
+  commit();
+
+  B.only(() => {
+    const handB = declassify(interact.getHand());
+  });
+  B.publish(handB);
+
+  const outcome = (handA + (4 - handB)) % 3;
+  commit();
+
+  each([A, B], () => {
+    interact.seeOutcome(outcome);
+  });
+  exit();
 
 });
